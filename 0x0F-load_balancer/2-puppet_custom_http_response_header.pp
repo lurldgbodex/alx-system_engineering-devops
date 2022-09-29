ca8f0e1ad5b25f4d5b65a140be5c2c41e6a0b447
Lurldgbodex <professionaleditor6@gmail.com>
2022-09-29 06:52:10 -0700

    add file 2-puppet_xustom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+nstall and add custom header to nginx
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file_line { 'add_header':
+  ensure  => 'present',
+  require => Package['nginx'],
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'root /var/www/html;',
+  line    => 'add_header X-Served-By $HOSTNAME;',
+  notify  =>  Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File_line['add_header'],
+}
